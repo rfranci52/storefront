5771d6b30bd6b0ec85d3b069486e072a91a18f6d
Rakim francis <rakimfrancis@Rakims-MBP-2.home>
2019-06-19 08:29:21 -0400

    styled the navbar component

--- thestore/src/components/Navbar.js
+++ thestore/src/components/Navbar.js
@@ -1,19 +1,49 @@
 import React, {Component} from "react"
 import {Link} from "react-router-dom"
 import logo from '../logo.svg';
+import styled from "styled-components"
 
 
 
 export default class NavBar extends Component{
     render(){
         return(
-           <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+           <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
                <Link to="/">
-                   <img src={logo} alt="store"/>
+                   <img src={logo} alt="store" className="navbar-brand"/>
                    <h3>Home</h3>
-                   
+               </Link>
+               <ul className="navbar-nav align items-center">
+               <li className="nav-item ml-5">
+                   <Link to="/" className="nav-link">
+                       products
+                   </Link>
+               </li>
+               </ul>
+               <Link to="/cart" className="ml-auto"><ButtonContainer>
+                   <span className="mr-2"><i className="fas fa-cart-plus">my cart</i></span>
+               </ButtonContainer>
                </Link>
            </nav>
         )
     }
 }
+const ButtonContainer = styled.button`
+text-transform:capitalize;
+font-size:1.4rem;
+background: transparent;
+border:0.05rem solid var(--lightBlue);
+color:var(--lightBlue)
+border-radius:0.5rem;
+padding: 0.2rem 0.5rem;
+cursor:pointer;
+margin:0.2rem 0.2rem 0;
+transition:all 0.5s ease-in-out;
+&:hover{background:(var--lightBlue)
+color:var(--mainBlue);
+};
+&:focus{
+    outline: none;
+}
+
+`;
